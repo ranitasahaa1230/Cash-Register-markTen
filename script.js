46314de6666ede378b2475c8f5e2342c6a4e6b5d
Ranita Saha <37957073+ranitasahaa1230@users.noreply.github.com>
2021-09-14 17:32:28 +0530

    error done

--- script.js
+++ script.js
@@ -1,43 +1,70 @@
 const billAmount=document.querySelector('#bill-amount');
 const cashAmount=document.querySelector('#cash-amount');
+const nextBtn=document.querySelector('#next-btn');
 const submitBtn=document.querySelector('#submit-btn');
-const outputEl=document.querySelector('#output-box');
+const cashGivenDiv=document.querySelector('.cash-given');
+const changeReturnDiv = document.querySelector(".changeReturn");
+const message=document.querySelector('#error-message');
+const Notes=document.querySelectorAll('.no-of-notes');
 
 const availableNotes=[2000,500,100,20,10,5,1];
 
-submitBtn.addEventListener('click', validateBillAmountAndCashValue);
-
-function validateBillAmountAndCashValue(){
+nextBtn.addEventListener('click',()=>{
     hideMessage();
-    if(billAmount.value>0){
-        if(cashAmount.value >= billAmount.value){
-            const amountReturned = cashAmount.value - billAmount.value;
-            calculateChange(amountReturned);
-
-        }else{
-            showMessage("Do you wanna wash plates?");
-        }
+    if(Number(billAmount.value)>0){
+        nextBtn.style.display="none";
+        cashGivenDiv.style.display="block";
     }else{
-        showMessage("Invalid Bill Amount");
-        
+        showMessage("Enter valid bill amount");
     }
+})
+
+submitBtn.addEventListener('click', validateBillAndCashAmount);
+
+function validateBillAndCashAmount(){
+    hideMessage();
+
+    let billAmtValue= Number(billAmount.value);
+    let cashGivenValue= Number(cashAmount.value);
+
+    if(billAmtValue>0 && cashGivenValue>0){
+
+        if(!Number.isInteger(cashGivenValue)){
+            showMessage("Enter valid amount in cash given field");
+            return;
+        }
+        if(billAmtValue > cashGivenValue){
+            showMessage("Cash is less than bill, please enter right amount");
+            return;
+        }
+        //if input valid calculate no. of notes
+        calculateChange(billAmtValue, cashGivenValue);
+    }else{
+        showMessage("Enter valid bill amount and cash given to continue");
+        }
 }
 
-function hideMessage(message){
+function calculateChange(bill,cash){
+    let amountReturned=cash-bill;
+    changeReturnDiv.style.display="block";
+    for(let i=0;i<availableNotes.length;i++){
+        const numberOfNotes= Math.trunc(amountReturned / availableNotes[i]);
+        // console.log(numberOfNotes);
+        amountReturned%=availableNotes[i];
+        Notes[i].innerText = numberOfNotes;
+    }
+
+}
+
+function showMessage(msg){
+    // console.log("here")
+    message.style.display="block";
+    message.innerText=msg;
+    cashGivenDiv.style.display="none";
+}
+
+function hideMessage(){
     message.style.display="none";
 
 }
 
-function showMessage(message){
-    message.style.display="block";
-    message.innerText="The Bill Amount should be greater than 0"
-}
-
-function calculateChange(amountReturned){
-    for(let i=0;i<availableNotes.length;i++){
-        const numberOfNotes=Math.trunc(amountReturned/availableNotes[0]);
-        amountReturned%=availableNotes[0];
-        numberOfNotes[i].innerText=numberOfNotes;
-    }
-
-}
