2c6d85b41c1a1c2524304632e04d064177c84794
Ranita Saha <37957073+ranitasahaa1230@users.noreply.github.com>
2021-09-26 18:21:33 +0530

    error handled

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const billAmount=document.querySelector('#bill-amount');
 const cashAmount=document.querySelector('#cash-amount');
+
 const nextBtn=document.querySelector('#next-btn');
 const submitBtn=document.querySelector('#submit-btn');
-const cashGivenDiv=document.querySelector('.cash-given');
-const changeReturnDiv = document.querySelector(".change-return");
+
+const cashGivenDiv=document.querySelector('.cashGivenInput');
+const changeReturnDiv = document.querySelector(".changeReturn");
+
+const output= document.querySelector("#output");
+
 const message=document.querySelector('#error-message');
 const Notes=document.querySelectorAll('.no-of-notes');
 
 const availableNotes=[2000,500,100,20,10,5,1];
 
+
 nextBtn.addEventListener('click', submitHandler);
 
+//if bill amt filled, display cash given input field
 function submitHandler(){
     hideMessage();
     if(Number(billAmount.value)>0){
@@ -21,9 +28,11 @@ function submitHandler(){
     }
 }
 
+//check btn clicked handler
 submitBtn.addEventListener('click', validateBillAndCashAmount);
 
 function validateBillAndCashAmount(){
+    clearNoOfNotes();
     hideMessage();
 
     let billAmtValue= Number(billAmount.value);
@@ -35,7 +44,7 @@ function validateBillAndCashAmount(){
             showMessage("Enter valid amount in cash given field");
             return;
         }
-        else if(billAmtValue > cashGivenValue){
+        if(billAmtValue > cashGivenValue){
             showMessage("Cash is less than bill, please enter right amount");
             return;
         }
@@ -46,9 +55,22 @@ function validateBillAndCashAmount(){
         }
 }
 
+//if check button clicked without refreshing the page, clear the no of notes values on the screen
+function clearNoOfNotes(){
+    for(let notes of availableNotes){
+        notes.innerText = "";
+    }
+}
+
+//to calculate no. of notes
 function calculateChange(bill,cash){
     let amountReturned=cash-bill;
+    if(amountReturned<1){
+        showMessage("No amount should be returned");
+        return;
+    }
     changeReturnDiv.style.display="block";
+
     for(let i=0;i<availableNotes.length;i++){
         const numberOfNotes= Math.trunc(amountReturned / availableNotes[i]);
         // console.log(numberOfNotes);
@@ -62,7 +84,7 @@ function showMessage(msg){
     // console.log("here")
     message.style.display="block";
     message.innerText=msg;
-    cashGivenDiv.style.display="none";
+    changeReturnDiv.style.display = "none";
 }
 
 function hideMessage(){
